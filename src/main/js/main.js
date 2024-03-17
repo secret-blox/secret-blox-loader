7009b34e8f8ee5376dda786a8f91089b3c48fd8f
111 <148693477+memoryapi@users.noreply.github.com>
2024-03-17 16:51:28 +0000

    fixed ctrl + s to save

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -907,7 +907,7 @@ injectButton.addEventListener('click', async () => {
 })
 
 document.addEventListener('keydown', function(event) {
-    if (event.ctrlKey && event.key === 's') {
+    if (event.ctrlKey || metaKey && event.key === 's') {
         event.preventDefault();
         Misc.saveFile();
     }
